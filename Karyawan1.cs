9fec9d12b35f2c1369f0d09520f182ef45757152
raihan7087 <48464609+raihan7087@users.noreply.github.com>
2019-05-02 19:42:57 +0700

    Add files via upload

---
+++ Karyawan1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSPEMROGRAMAN
+{
+    class Karyawan1
+    {
+        public string nama { get; set; }
+        public string nik { get; set; }
+        public int gaji { get; set; }
+        public string nama2 { get; set; }
+        public string nik2 { get; set; }
+        public int gaji2 { get; set; }
+        public void infoGaji()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("     Gaji anda bulan ini :");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("No    Nik              Nama               Total Gaji ");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("1    {0}       {1}            {2}    ", nik, nama, gaji);
+            Console.WriteLine("2    {0}       {1}           {2}    ", nik2, nama2, gaji2);
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine();
+        }
+        public void kenaikanGaji()
+        {
+            if (gaji < 0)
+            {
+                gaji = 0;
+            }
+            else if (gaji2 < 0)
+            {
+                gaji2 = 0;
+            }
+            Console.WriteLine("     Gaji anda setelah mendapat Bonus :");
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("No    Nik            Nama                Gaji + Bonus  ");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("1     {0}      {1}            {2}      ", nik, nama, gaji + (gaji * 0.1));
+            Console.WriteLine("2     {0}      {1}           {2}      ", nik2, nama2, gaji2 + (gaji2 * 0.1));
+            Console.WriteLine("--------------------------------------------------------");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSPEMROGRAMAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan1 krywn = new Karyawan1();
+
+            krywn.nama = "MBAH TEJO";
+            krywn.nik = "770841294";
+            krywn.gaji = 2000000;
+
+            krywn.nama2 = "BANG KODIR";
+            krywn.nik2 = "770841293";
+            krywn.gaji2 = 3000000;
+
+            krywn.infoGaji();
+            Console.WriteLine();
+            krywn.kenaikanGaji();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
